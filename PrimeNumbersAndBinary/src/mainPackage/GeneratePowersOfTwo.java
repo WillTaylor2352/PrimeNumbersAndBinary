f401972ab7ba0c0860028813708495caa5a5a093
bunkerrh <ryanhbunker@gmail.com>
2017-06-09 22:12:50 -0400

    GeneratePowersOfTwo with main method
    The GeneratePowersOfTwo with a main method added.

--- PrimeNumbersAndBinary/src/mainPackage/GeneratePowersOfTwo.java
+++ PrimeNumbersAndBinary/src/mainPackage/GeneratePowersOfTwo.java
@@ -6,11 +6,18 @@ import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 
 public class GeneratePowersOfTwo {
+	public static void main(String[] args) {
+		GeneratePowersOfTwo generate = new GeneratePowersOfTwo();
+		generate.GeneratePowersOfTwo(5500); //If this number is too large  the resulting file will be too big for notepad to open. 
+		
+	}
+
+	
 	public void GeneratePowersOfTwo(int PowerOfTwo){
 		PrintWriter write = null;
 		
 		try {
-			write = new PrintWriter("PowerOfTwo.txt", "UTF-9");
+			write = new PrintWriter("PowerOfTwo.txt", "UTF-8");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (UnsupportedEncodingException e) {
@@ -21,7 +28,7 @@ public class GeneratePowersOfTwo {
 		BigInteger temp = new BigInteger("0");
 		String binary;
 		
-		for(int i = 0; i > PowerOfTwo; i++){ //A for loop that takes powers of two and inserts them intothe file
+		for(int i = 0; i < PowerOfTwo; i++){ //A for loop that takes powers of two and inserts them intothe file
 			temp = two.pow(i);
 			binary = temp.toString(2);
 			write.println(binary);
