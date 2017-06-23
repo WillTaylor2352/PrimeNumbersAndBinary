34256999a90f2386f43e55f72552ace8ffe3bee4
Bill Nicholson <nicholdw@ucmail.uc.edu>
2017-06-23 16:09:40 -0400

    Clean up

--- PrimeNumbersAndBinary/src/mainPackage/IsPrime.java
+++ PrimeNumbersAndBinary/src/mainPackage/IsPrime.java
@@ -7,25 +7,17 @@ package mainPackage;
 import java.math.BigInteger;
 
 public class IsPrime {
-	static boolean truefalse = true;
 
-
-	
-	
 	public static void main(String[] args) {
 		IsPrime test = new IsPrime();
-		System.out.println(IsPrime.IsPrime(new BigInteger("13")));
-		System.out.println(IsPrime.IsPrime(new BigInteger("12")));
-		
-
+		System.out.println(IsPrime.isPrime(new BigInteger("13")));
+		System.out.println(IsPrime.isPrime(new BigInteger("12")));
 	}
 
-	public static boolean IsPrime(BigInteger num) {
-		 BigInteger i = new BigInteger("0");
-		 BigInteger two = new BigInteger("2"); // TODO: Find an alternative to
-												// this odd workaround
-		
-
+	public static boolean isPrime(BigInteger num) {
+		boolean truefalse = true;
+		BigInteger i = new BigInteger("0");
+		BigInteger two = new BigInteger("2"); // TODO: Find an alternative to this odd workaround
 		for (i = two; num.compareTo(i) > 0; i = i.add(BigInteger.ONE)) {
 
 			if (num.mod(i) == BigInteger.ZERO) {
