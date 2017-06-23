7add6299006b9ff186da14a28fcb2d822cc86724
Bill Nicholson <nicholdw@ucmail.uc.edu>
2017-06-23 16:22:06 -0400

    Added stub for JUnit test on IsPrime class

---
+++ PrimeNumbersAndBinary/src/mainPackage/IsPrimeTest.java
@@ -0,0 +1,14 @@
+package mainPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IsPrimeTest {
+
+	@Test
+	public void testIsPrime() {
+		fail("Not yet implemented");
+	}
+
+}
