9bdaeed397b0d36f037bbb34503bec8eedc18676
Bill Nicholson <nicholdw@ucmail.uc.edu>
2017-06-29 17:17:20 -0400

    Clean up

--- PrimeNumbersAndBinary/src/mainPackage/ConvertBaseTenToBaseTwo.java
+++ PrimeNumbersAndBinary/src/mainPackage/ConvertBaseTenToBaseTwo.java
@@ -1,3 +1,6 @@
+/*
+ * Don't know who wrote this
+ */
 package mainPackage;
 
 import java.io.File;
@@ -7,45 +10,34 @@ import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
 
 public class ConvertBaseTenToBaseTwo {
-	
+
 	public static void main(String[] args) {
 		ConvertBaseTenToBaseTwo convert = new ConvertBaseTenToBaseTwo();
 		convert.ConvertBaseTenToBaseTwo();
-		
-	}
 
+	}
 	void ConvertBaseTenToBaseTwo() {
 		File file = new File("unconverted.txt");
-		
+
 		Scanner input = null;
 		try {
 			input = new Scanner(file);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		} 
-		
-		
+		}
 		PrintWriter writer = null;
 		try {
 			writer = new PrintWriter("PrimeNumbersConverted.txt", "UTF-8");
-		} catch (FileNotFoundException | UnsupportedEncodingException e) {
-			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
-		
 		while (input.hasNext()) {
-
-			int number = input.nextInt(); // converts the number integer into
-											// a binary string
-
+			int number = input.nextInt(); // converts the number integer into a binary string
 			writer.println(Integer.toBinaryString(number));
-
 		}
-
 		input.close(); // closes the scanner.
 		writer.close();
-	
-
 	}
-
 }
